fd5944bc9d61b79a97e21d1d93d05f9213ea8d2b
Soumyadeep Mondal <soumyadeepmondal@MacBookAir.bbrouter>
2025-09-23 13:35:03 +0530

    minor: comment out mentor and newsletter section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,14 +2,13 @@ import React from 'react'
 import dynamic from 'next/dynamic'
 import { NextPageWithLayout } from '@/interfaces/layout'
 import { MainLayout } from '@/components/layout'
-// import { HomeFeature, HomeHero, HomePopularCourse, HomeTestimonial, HomeOurMentors, DynamicHomeNewsLetter } from '@/components/home'
 
 const DynamicHomeHero = dynamic(() => import('../components/home/hero'))
 const DynamicHomeFeature = dynamic(() => import('../components/home/feature'))
 const DynamicHomePopularCourse = dynamic(() => import('../components/home/popular-courses'))
 const DynamicHomeTestimonial = dynamic(() => import('../components/home/testimonial'))
-const DynamicHomeOurMentors = dynamic(() => import('../components/home/mentors'))
-const DynamicHomeNewsLetter = dynamic(() => import('../components/home/newsletter'))
+// const DynamicHomeOurMentors = dynamic(() => import('../components/home/mentors'))
+// const DynamicHomeNewsLetter = dynamic(() => import('../components/home/newsletter'))
 
 const Home: NextPageWithLayout = () => {
   return (
@@ -18,8 +17,8 @@ const Home: NextPageWithLayout = () => {
       <DynamicHomePopularCourse />
       <DynamicHomeFeature />
       <DynamicHomeTestimonial />
-      <DynamicHomeOurMentors />
-      <DynamicHomeNewsLetter />
+      {/* <DynamicHomeOurMentors /> */}
+      {/* <DynamicHomeNewsLetter /> */}
     </>
   )
 }
