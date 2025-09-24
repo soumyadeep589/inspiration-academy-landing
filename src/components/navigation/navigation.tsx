d0e727465d963ae312e7372616cd46b6580bc1df
Soumyadeep Mondal <soumyadeepmondal@MacBookAir.bbrouter>
2025-09-24 19:28:37 +0530

    chore: change anchor with muilink

--- src/components/navigation/navigation.tsx
+++ src/components/navigation/navigation.tsx
@@ -1,4 +1,6 @@
 import Box from '@mui/material/Box';
+import MuiLink from '@mui/material/Link';
+import Link from 'next/link';
 import { useRouter } from "next/router";
 import { FC } from 'react';
 import { navigations } from './navigation.data';
@@ -14,44 +16,46 @@ const Navigation: FC<NavigationProps> = ({ onLinkClick }) => {
       {navigations.map(({ path: destination, label }) => {
         const isActive = router.asPath === destination;
         return (
-          <a href={destination} key={destination}>
-            <Box
-              onClick={onLinkClick}
-              sx={{
-                position: 'relative',
-                color: isActive ? "primary.main" : "text.disabled",
-                cursor: 'pointer',
-                fontWeight: 600,
-                display: 'inline-flex',
-                alignItems: 'center',
-                justifyContent: 'center',
-                px: { xs: 0, md: 3 },
-                mb: { xs: 3, md: 0 },
-                fontSize: { xs: '1.2rem', md: 'inherit' },
-                '& > div': { display: 'none' },
-                '&:hover': {
-                  color: 'primary.main',
-                  '&>div': {
-                    display: 'block',
-                  },
-                },
-              }}
-            >
-
+          <Link href={destination} key={destination} passHref>
+            <MuiLink>
               <Box
+                onClick={onLinkClick}
                 sx={{
-                  position: 'absolute',
-                  top: 12,
-                  transform: 'rotate(3deg)',
-                  '& img': { width: 44, height: 'auto' },
+                  position: 'relative',
+                  color: isActive ? "primary.main" : "text.disabled",
+                  cursor: 'pointer',
+                  fontWeight: 600,
+                  display: 'inline-flex',
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                  px: { xs: 0, md: 3 },
+                  mb: { xs: 3, md: 0 },
+                  fontSize: { xs: '1.2rem', md: 'inherit' },
+                  '& > div': { display: 'none' },
+                  '&:hover': {
+                    color: 'primary.main',
+                    '&>div': {
+                      display: 'block',
+                    },
+                  },
                 }}
               >
-                {/* eslint-disable-next-line */}
-                <img src="/images/headline-curve.svg" alt="Headline curve" />
+
+                <Box
+                  sx={{
+                    position: 'absolute',
+                    top: 12,
+                    transform: 'rotate(3deg)',
+                    '& img': { width: 44, height: 'auto' },
+                  }}
+                >
+                  {/* eslint-disable-next-line */}
+                  <img src="/images/headline-curve.svg" alt="Headline curve" />
+                </Box>
+                {label}
               </Box>
-              {label}
-            </Box>
-          </a>
+            </MuiLink>
+          </Link>
         )
       }
 
